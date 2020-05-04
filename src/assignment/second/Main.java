6135cf6f0d762e2fcedfb21605d5b26fa045ab4b
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-05-05 00:04:15 +0530

    main

--- src/assignment/second/Main.java
+++ src/assignment/second/Main.java
@@ -1,11 +1,9 @@
 package assignment.second;
 
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         PriorityQueue<Student> p = new PriorityQueue<>();
         Student s1 = new Student("abc", 7.5);
         p.add(s1);
@@ -15,7 +13,7 @@ public class Main {
         p.add(s3);
         System.out.println("Students in Priority Order");
         while (!p.isEmpty()) {
-            System.out.println();
+            System.out.println(p.peek().getName());
         }
     }
 }

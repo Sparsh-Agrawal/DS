c5496362bf6d0ea00ecc073fc0c943cbcd71b525
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-05-04 23:53:45 +0530

    queue

--- src/assignment/second/PriorityQueue.java
+++ src/assignment/second/PriorityQueue.java
@@ -1,7 +1,11 @@
 package assignment.second;
 
-public class PriorityQueue {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class PriorityQueue<S> {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Student s = new Student("abc", 7.5);
+        PriorityQueue<Student> p = new PriorityQueue<>();
     }
 }

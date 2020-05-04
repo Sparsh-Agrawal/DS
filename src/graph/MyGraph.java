9998ba22b2f480b2b41cd34fbc687600269fbf49
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-05-04 14:35:24 +0530

    graph

--- src/graph/MyGraph.java
+++ src/graph/MyGraph.java
@@ -37,5 +37,20 @@ public class MyGraph {
             System.out.println();
         }
     }
+
+    public static void main(String[] args) {
+        int maxNode = 5;
+        MyGraph graph = new MyGraph(maxNode);
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(0,3);
+        graph.addEdge(1,4);
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+
+        graph.display();
+
+        System.out.println(graph.isEdge(2,1));
+    }
 }
 

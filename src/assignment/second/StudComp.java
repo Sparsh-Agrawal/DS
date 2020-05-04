9f7c7a9c87f0c3f78d2dea0398702ac12ce99937
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-05-04 23:48:16 +0530

    student compare

--- src/assignment/second/StudComp.java
+++ src/assignment/second/StudComp.java
@@ -1,5 +1,17 @@
 package assignment.second;
 
-public class StudComp {
+import java.util.Comparator;
 
+public class StudComp  implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        if(o1.getCgpa()>o2.getCgpa()) {
+            return -1;
+        }
+        else if(o1.getCgpa()<o2.getCgpa()) {
+            return 1;
+        }
+        return 0;
+    }
 }

e938be0ebfba5def088bfef148ddfb7816fa1b95
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-05-04 23:57:03 +0530

    main

--- src/assignment/second/Main.java
+++ src/assignment/second/Main.java
@@ -1,11 +1,13 @@
 package assignment.second;
 
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class PriorityQueue<S> {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Student s = new Student("abc", 7.5);
         PriorityQueue<Student> p = new PriorityQueue<>();
+        Student s1 = new Student("abc", 7.5);
+        p.add(s1);
     }
 }

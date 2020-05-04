d82d07858591acb5d0748313d22eb137c70da572
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-05-04 14:28:18 +0530

    graph

--- src/graph/MyGraph.java
+++ src/graph/MyGraph.java
@@ -9,6 +9,12 @@ public class MyGraph {
 
     }
 
-    //ad
+    //add a new edge
+    public void addEdge(int n1, int n2) {
+        if(n1 < maxNodes && n2 < maxNodes) {
+            adjacencyMatrix[n1][n2] = 1;
+            adjacencyMatrix[n2][n1] = 1;
+        }
+    }
 }
 

f5b140e09eeea58c3fcddfc2a52b73dcca710bf6
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-05-04 14:30:21 +0530

    graph

--- src/graph/MyGraph.java
+++ src/graph/MyGraph.java
@@ -16,5 +16,15 @@ public class MyGraph {
             adjacencyMatrix[n2][n1] = 1;
         }
     }
+
+    //check if there is edge
+    public  boolean isEdge(int n1, int n2) {
+        boolean res = false;
+        if(n1 < maxNodes && n2 < maxNodes) {
+            if(adjacencyMatrix[n1][n2]==1) {
+                res = true;
+            }
+        }
+    }
 }
 

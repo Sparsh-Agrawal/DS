57bcfeca472b9e63f3866070065bb4fbf0eb4cda
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-05-04 14:27:02 +0530

    graph

--- src/graph/MyGraph.java
+++ src/graph/MyGraph.java
@@ -2,4 +2,13 @@ package graph;
 
 public class MyGraph {
     private int[][] adjacencyMatrix;
+    private  int maxNodes;
+
+    public MyGraph(int maxNodes) {
+        this.maxNodes = maxNodes;
+
+    }
+
+    //ad
 }
+
